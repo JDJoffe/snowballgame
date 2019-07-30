00e46504008e75d215b69853944c989c041cbe49
josh <48034855+JDJoffe@users.noreply.github.com>
2019-07-30 12:11:07 +1000

    initial

---
+++ MobileTopDownShooter/Assets/Scripts/SnowballSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballSpawn : MonoBehaviour
+{
+    public GameObject[] wayPoint;
+    public GameObject snowBall;
+    public int num;
+    public float spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > 3)
+        {
+            Spawner();
+            spawnTimer = 0;
+        }
+        
+    }
+    public void Spawner()
+    {
+       num = Random.Range(0,3);
+
+       
+        Instantiate(snowBall, wayPoint[num].transform.position, Quaternion.identity);
+    }
+   
+}
